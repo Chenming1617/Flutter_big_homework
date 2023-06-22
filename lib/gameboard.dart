6d7531d68e835178812f23cc4ff50e3af0ff4a8d
Singawe Jack <chenming@mail.ynu.edu.cn>
2023-06-22 13:34:02 +0000

    add zhou

--- lib/gameboard.dart
+++ lib/gameboard.dart
@@ -1,7 +1,22 @@
+import 'dart:async';
+import 'dart:ffi';
 import 'dart:html';
-
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:gitpod_flutter_quickstart/piece.dart';
 import 'package:gitpod_flutter_quickstart/pixel.dart';
+import 'package:gitpod_flutter_quickstart/values.dart';
+import 'piece.dart';
+import 'pixel.dart';
+
+//创建2X2的区域
+List<List<Tetromino?>> gameBoard = List.generate(
+  colLength,
+  (i) => List.generate(
+    rowLength,
+    (j) => null,
+  ),
+);
 
 class GameBoard extends StatefulWidget {
   const GameBoard({super.key});
@@ -15,23 +30,106 @@ class _GameBoardState extends State<GameBoard> {
   int rowLength = 10;
   int colLength = 15;
 
+  Piece currentPiece = Piece(type: Tetromino.L);
+  @override
+  void initState() {
+    super.initState();
+
+    startGame();
+  }
+
+  void startGame() {
+    currentPiece.initializePiece();
+    //下降速度设置
+    Duration frameRate = const Duration(milliseconds: 500);
+    gameLoop(frameRate);
+  }
+
+  void gameLoop(Duration frameRate) {
+    Timer.periodic(
+      frameRate,
+      (timer) {
+        setState(() {
+          //向下移动，方块
+
+          currentPiece.movePiece(Direction.down);
+        });
+      },
+    );
+  }
+
+//检测位置是否有冲突
+  bool checkCollision(Direction direction) {
+    for (int i = 0; i < currentPiece.position.length; i++) {
+      //计算位置
+      int row = (currentPiece.position[i] / rowLength).floor();
+      int col = currentPiece.position[i] % rowLength;
+      //基于方向判断行和列
+      if (direction == Direction.left) {
+        col -= 1;
+      } else if (direction == Direction.right) {
+        col += 1;
+      } else if (direction == Direction.down) {
+        col += 1;
+      }
+      //检查是否出界
+      if (row >= colLength || col < 0 || col >= rowLength) {
+        return true;
+      }
+    }
+    //出界了
+    return false;
+  }
+
+  void checkLanding() {
+    //如果下面有方块
+    if (checkCollision(Direction.down)) {
+      for (int i = 0; i < currentPiece.position.length; i++) {
+        int row = (currentPiece.position[i] / rowLength).floor();
+        int col = currentPiece.position[i] % rowLength;
+        if (row >= 0 && col >= 0) {
+          gameBoard[row][col] = currentPiece.type;
+        }
+      }
+
+      //一旦着落，创建其他的方块
+      createNewPiece();
+    }
+  }
+
+  void createNewPiece() {
+    //创建随机对象来生成随机值
+    Random rand = Random();
+    //创建一个人随机类型的方块
+    Tetromino randomType =
+        Tetromino.values[rand.nextInt(Tetromino.values.length)];
+    currentPiece = Piece(type: randomType);
+    currentPiece.initializePiece();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey,
       body: GridView.builder(
-        itemCount: rowLength*colLength,
+        itemCount: rowLength * colLength,
         physics: const NeverScrollableScrollPhysics(),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: rowLength),
-       itemBuilder: (context,index)=>Pixel(
-        color: Colors.grey[900],
-        child: index,
-       ),
-      
+        itemBuilder: (context, index) {
+          if (currentPiece.position.contains(index)) {
+            return Pixel(
+              color: Colors.yellow,
+              child: index,
+            );
+          } else {
+            return Pixel(
+              color: Colors.grey[900],
+              child: index,
+            );
+          }
+        },
       ),
     );
-
   }
 }
--- lib/piece.dart
+++ lib/piece.dart
@@ -6,5 +6,90 @@ class Piece {
 
   List<int> position = [];
 
-  void initializepiece
+  void initializePiece() {
+    switch (type) {
+      case Tetromino.L:
+        position = [
+          -24,
+          -14,
+          -4,
+          -3,
+        ];
+        break;
+      case Tetromino.I:
+        position = [
+          -34,
+          -24,
+          -14,
+          -4,
+        ];
+        break;
+      case Tetromino.J:
+        position = [
+          -24,
+          -14,
+          -4,
+          -5,
+        ];
+        break;
+      case Tetromino.O:
+        position = [
+          -24,
+          -25,
+          -14,
+          -15,
+          
+        ];
+        break;
+      case Tetromino.S:
+        position = [
+          -24,
+          -23,
+          -14,
+          -15,
+        ];
+        break;   
+
+      case Tetromino.Z:
+        position = [
+          -24,
+          -23,
+          -13,
+          -12,
+        ];
+          break; 
+      case Tetromino.T:
+        position = [
+          -24,
+          -14,
+          -13,
+          -4,
+        ];
+        break;    
+         
+      default:
+        break;
+    }
+  }
+
+  void movePiece(Direction direction) {
+    switch (direction) {
+      case Direction.down:
+        for (int i = 0; i < position.length; i++) {
+          position[i] += rowLength;
+        }
+        break;
+      case Direction.left:
+        for (int i = 0; i < position.length; i++) {
+          position[i] -= 1;
+        }
+        break;
+      case Direction.right:
+        for (int i = 0; i < position.length; i++) {
+          position[i] += 1;
+        }
+        break;
+      default:
+    }
+  }
 }
